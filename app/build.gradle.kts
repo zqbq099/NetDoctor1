a5993335e980c809cd3c68bbfde6ebc47a1239ff
zqbq099 <zqbq099@gmail.com>
2026-04-15 02:46:47 +0300

    Downgrade SDK versions and update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,26 +5,32 @@ plugins {
 
 android {
     namespace = "com.netdoctor"
-    compileSdk = 34
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "com.netdoctor"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
     }
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
     }
 }
 
 dependencies {
+    implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
-    implementation("androidx.cardview:cardview:1.0.0")
-    implementation("com.google.android.gms:play-services-ads:22.5.0")
+    implementation("com.google.android.material:material:1.8.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 }
